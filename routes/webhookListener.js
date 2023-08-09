ebacb4e370578175e86728bbf2fc547d77a47e2f
Talal <alamtalal004@gmail.com>
2023-08-09 04:32:38 -0400

    Removed the brakets from the emit event and returned a response to the request

--- routes/webhookListener.js
+++ routes/webhookListener.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { io } = require("socket.io-client");
 const socket = io("https://bolt-ai-chatbot.onrender.com");
+//const socket = io("http://localhost:3001");
 
 router.post("/get", (req, res) => {
   //Send the alert to the axios Server
   const message = req.body;
-  socket.emit("webStatus", { message });
-  console.log(req.body);
+  socket.emit("webStatus", message);
+  res.status(200).send("OK");
 });
 
 module.exports = router;
